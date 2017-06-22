a232e02528126f473da0fed44cfd58cb2f3fae08
hdorothea <dorothea.hormann@gmail.com>
2017-06-22 18:26:28 -0400

    make director be its own call again

--- src/app/movie-detail-view/movie-detail-view.component.ts
+++ src/app/movie-detail-view/movie-detail-view.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
     <div *ngIf="movie">
       <div class="title"> {{ movie.title }} <span class="year"> {{ movie.year }} </span> </div>
       <div class="tagline"> {{ movie.tagline }} </div>
-      <app-director-link *ngFor="let director of movie.directors" [director]="director"></app-director-link>
+      <app-director-link [movieId]="movie.id"></app-director-link>
       <div class="overview"> {{ movie.overview }}</div>
       <img [src]="movie.posterPath"/>
     </div>
