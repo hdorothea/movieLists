9440dd58c0749cddcce3382af7f28a7d184a582e
hdorothea <dorothea.hormann@gmail.com>
2017-06-22 18:29:10 -0400

    frontend id for lists is same as database id

--- backend/db/create_tables.sql
+++ backend/db/create_tables.sql
@@ -8,7 +8,7 @@ CREATE SCHEMA movielistsdb;
     );
 
     CREATE TABLE movielistsdb.lists (
-        id VARCHAR(100) NOT NULL,
+        id VARCHAR(200) NOT NULL PRIMARY KEY,
         owner_id INTEGER,
         name VARCHAR(50) NOT NULL,
         creator_cookie VARCHAR(100)
@@ -16,5 +16,5 @@ CREATE SCHEMA movielistsdb;
 
     CREATE TABLE movielistsdb.movies (
         movie_id INTEGER,
-        movielist_id INTEGER
+        movielist_id VARCHAR(200) NOT NULL
     );
--- backend/db/fill_tables.sql
+++ backend/db/fill_tables.sql
@@ -1,8 +1,10 @@
-INSERT INTO movielistsdb.users(name, password_hash) VALUES('dorothea', 'abcdef');
-INSERT INTO movielistsdb.users(name, password_hash) VALUES('lola', '123456');
-INSERT INTO movielistsdb.lists(owner_id, name) VALUES(1, 'awfull movies');
-INSERT INTO movielistsdb.lists(owner_id, name) VALUES(2, 'good movies');
-INSERT INTO movielistsdb.movies(movie_id, movielist_id) VALUES(700, 1);
-INSERT INTO movielistsdb.movies(movie_id, movielist_id) VALUES(600, 1);
-INSERT INTO movielistsdb.movies(movie_id, movielist_id) VALUES(8888, 2);
+INSERT INTO movielistsdb.users(name, password_hash) VALUES('dorothea', 'abcdefgh');
+INSERT INTO movielistsdb.users(name, password_hash) VALUES('lolalola', '12345678');
+INSERT INTO movielistsdb.lists(id, owner_id, name) VALUES('a5999339assf', 1, 'awfull movies');
+INSERT INTO movielistsdb.lists(id, owner_id, name) VALUES('a1dkdkdkdkdk', 1, 'weird movies');
+INSERT INTO movielistsdb.lists(id, owner_id, name) VALUES('alalalallala', 2, 'good movies');
+INSERT INTO movielistsdb.movies(movie_id, movielist_id) VALUES(700, 'a5999339assf');
+INSERT INTO movielistsdb.movies(movie_id, movielist_id) VALUES(600, 'a5999339assf');
+INSERT INTO movielistsdb.movies(movie_id, movielist_id) VALUES(600, 'a1dkdkdkdkdk');
+INSERT INTO movielistsdb.movies(movie_id, movielist_id) VALUES(8888,'alalalallala');
 
